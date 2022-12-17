be5a76a052bdce8467435de83654c9a32454d9a9
Nitsevich Artur <arturnitsevich938@gmail.com>
2022-12-17 15:59:54 +0300

    HotKeys input into datagrid(unfinished)

--- SourceCodeEditor/UserControls/Options/HotKeysOptionsControl.Designer.cs
+++ SourceCodeEditor/UserControls/Options/HotKeysOptionsControl.Designer.cs
@@ -31,7 +31,11 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Action = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.HotKey = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonDiscard = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -50,6 +54,7 @@
             this.dataGridView1.RowTemplate.Height = 29;
             this.dataGridView1.Size = new System.Drawing.Size(581, 469);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView1_KeyDown);
             // 
             // Action
             // 
@@ -57,6 +62,7 @@
             this.Action.MinimumWidth = 6;
             this.Action.Name = "Action";
             this.Action.ReadOnly = true;
+            this.Action.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             this.Action.Width = 125;
             // 
             // HotKey
@@ -64,17 +70,51 @@
             this.HotKey.HeaderText = "ColumnHotKeys";
             this.HotKey.MinimumWidth = 6;
             this.HotKey.Name = "HotKey";
+            this.HotKey.ReadOnly = true;
+            this.HotKey.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             this.HotKey.Width = 125;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(484, 6);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(94, 29);
+            this.buttonSave.TabIndex = 1;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonDiscard
+            // 
+            this.buttonDiscard.Location = new System.Drawing.Point(384, 6);
+            this.buttonDiscard.Name = "buttonDiscard";
+            this.buttonDiscard.Size = new System.Drawing.Size(94, 29);
+            this.buttonDiscard.TabIndex = 2;
+            this.buttonDiscard.Text = "Discard";
+            this.buttonDiscard.UseVisualStyleBackColor = true;
+            this.buttonDiscard.Click += new System.EventHandler(this.buttonDiscard_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonDiscard);
+            this.panel1.Controls.Add(this.buttonSave);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 431);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(581, 38);
+            this.panel1.TabIndex = 3;
             // 
             // HotKeysOptionsControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "HotKeysOptionsControl";
             this.Size = new System.Drawing.Size(581, 469);
             this.Load += new System.EventHandler(this.HotKeysOptionsControl_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -84,5 +124,8 @@
         private DataGridView dataGridView1;
         private DataGridViewTextBoxColumn Action;
         private DataGridViewTextBoxColumn HotKey;
+        private Button buttonSave;
+        private Button buttonDiscard;
+        private Panel panel1;
     }
 }
--- SourceCodeEditor/UserControls/Options/HotKeysOptionsControl.cs
+++ SourceCodeEditor/UserControls/Options/HotKeysOptionsControl.cs
@@ -19,6 +19,22 @@ namespace SourceCodeEditor.UserControls.Options
             _mainForm = mainForm;
         }
 
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == (Keys.Control | Keys.H))
+                return true;
+
+            if (keyData == (Keys.Control | Keys.A))
+                return true;
+            if (keyData == (Keys.Control | Keys.O))
+                return true;
+            if (keyData == (Keys.Control | Keys.D))
+                return true;
+
+
+            return base.ProcessCmdKey(ref msg, keyData); //we didn't handle it
+        }
+
         private void UploadDictionaryToDataGrid(Dictionary<string, Keys> pairs)
         {
             foreach (var item in pairs)
@@ -39,6 +55,53 @@ namespace SourceCodeEditor.UserControls.Options
             }
 
             UploadDictionaryToDataGrid(ActionHotkeyPairs);
+        }
+
+        private (bool,int,int) HotKeyExists(Keys hotKey)
+        {
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    if (dataGridView1.Rows[i].Cells[j].Value.ToString() == hotKey.ToString())
+                        return (true,i,j);
+                }
+            }
+            return (false,-1,-1);
+        }
+
+        private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (dataGridView1.CurrentCell.ColumnIndex != 0)
+            {
+                var HotKey = (Keys)Enum.Parse(typeof(Keys), e.KeyValue.ToString());
+                (bool, int, int) result = HotKeyExists(HotKey);
+
+                if (!result.Item1)
+                {
+                    /*if (e.Modifiers == Keys.ControlKey || e.Modifiers == Keys.ShiftKey)
+                    {
+                        var modifier = (Keys)Enum.Parse(typeof(Keys), e.Modifiers.ToString());
+                        dataGridView1.CurrentCell.Value = $"{modifier} + {HotKey}";
+                    }*/
+                    dataGridView1.CurrentCell.Value = HotKey;
+                }
+                else
+                {
+                    dataGridView1.Rows[result.Item2].Cells[result.Item3].Value = String.Empty;
+                    dataGridView1.CurrentCell.Value = HotKey;
+                }
+            }
+        }
+
+        private void buttonDiscard_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
